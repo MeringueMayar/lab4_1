1450b0e0c0ecdee1c625145cd04c28afc3fbca89
Bartosz Suśniło <susnilox@gmail.com>
2018-04-24 17:25:25 +0200

    Create BetterRadar

---
+++ src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,35 @@
+package edu.iis.mto.multithread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BetterRadar {
+
+    private PatriotBattery battery;
+
+    public BetterRadar(PatriotBattery missle) {
+        this.battery = missle;
+    }
+
+    public void notice(Scud enemyMissle) {
+        launchPatriot();
+    }
+
+    private void launchPatriot() {
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+            Runnable launchPatriotTask = new Runnable() {
+
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        battery.launchPatriot();
+                    }
+                }
+            };
+            executor.execute(launchPatriotTask);
+
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+    }
+}
