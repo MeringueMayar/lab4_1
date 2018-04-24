42291e1d41e8143543de91e6b12c6d787517bbb8
liwocha.dawid <202214@edu.p.lodz.pl>
2018-04-24 17:08:26 +0200

    added test class for BetterRadar

---
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -0,0 +1,26 @@
+package edu.iis.mto.multithread;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import edu.iis.mto.multithread.RepeatRule.Repeat;
+
+public class BetterRadarTest {
+    
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
+    @Repeat(times = 1000)
+    @Test
+    public void launchPatriotOnceWhenNoticesAScudMissle() {
+        PatriotBattery batteryMock = mock(PatriotBattery.class);
+        BetterRadar radar = new BetterRadar(batteryMock);
+        radar.notice(new Scud());
+        verify(batteryMock, times(10)).launchPatriot();
+    }
+    
+}
