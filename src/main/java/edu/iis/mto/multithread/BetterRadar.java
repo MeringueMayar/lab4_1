8bf171e9e1020d5cdee8ddc55f7300ce7511bf37
Wiktor Pabich <202258@edu.p.lodz.pl>
2018-04-24 17:30:07 +0200

    Added implementation of BetterRadar class

--- src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -1,5 +1,38 @@
 package edu.iis.mto.multithread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BetterRadar {
 
+    private PatriotBattery battery;
+    private int timesLaunched;
+
+    public BetterRadar(PatriotBattery missle) {
+        this.timesLaunched = 0;
+        this.battery = missle;
+    }
+
+    public void notice(Scud enemyMissle) {
+        launchPatriot();
+    }
+
+    private void launchPatriot() {
+
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        executor.execute( new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+
+                    battery.launchPatriot();
+                }
+            }
+        });
+
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+    }
 }
