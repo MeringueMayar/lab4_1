dbfe50824c0f00b400035f256105777c16794d63
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-05-22 17:47:20 +0200

    Poprawiona wersja odpowiedzialnosc przerzucona na test

--- src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -1,37 +1,34 @@
 package edu.iis.mto.multithread;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class BetterRadar {
-    private ExecutorService executor;
+
+    private Executor executor;
     private PatriotBattery battery;
-    
-    public BetterRadar (PatriotBattery battery, ExecutorService executor) {
-        this.battery=battery;
-        this.executor=executor;
-        
+
+    public BetterRadar(PatriotBattery battery, Executor executor) {
+        this.battery = battery;
+        this.executor = executor;
     }
+
     public void notice(Scud missle) {
         launchMissle();
     }
+
     private void launchMissle() {
-        executor.submit(() ->
-        {
-            for (int i = 0; i < 10; i++) {
-                battery.launchPatriot();
+        executor.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    battery.launchPatriot();
+                }
+
             }
         });
-        
-        try {
-            executor.shutdown();
-            executor.awaitTermination(5, TimeUnit.SECONDS);
-        }
-        catch(InterruptedException e){
-            System.err.println("Executor shutdown interupted");
-        }
-        finally {
-            executor.shutdownNow();
-        }
+
     }
 }
