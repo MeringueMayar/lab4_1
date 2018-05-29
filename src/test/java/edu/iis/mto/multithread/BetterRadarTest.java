69c1405658f7786a994876b59b726cc9117c9a25
Mateusz Sędkowski <mateusz.sedkowski@gmail.com>
2018-05-29 16:45:03 +0200

    Create BetterRadarTest and RepeatRule class.

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,17 +1,21 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Rule;
+import org.junit.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import org.junit.Test;
 
-public class RadarTest {
+public class BetterRadarTest {
+
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
 
     @Test
+    @RepeatRule.Repeat(times = 10000 )
     public void launchPatriotOnceWhenNoticesAScudMissile() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
         radar.notice(new Scud());
         verify(batteryMock).launchPatriot();
     }
-
 }
