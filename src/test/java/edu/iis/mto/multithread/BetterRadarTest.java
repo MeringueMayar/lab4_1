813c627781f6bb051658d8113c947e3ab5804fbc
Adstergo <dr4n3t@gmail.com>
2018-06-05 17:18:09 +0200

    Move code shared by tests to setUp method

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Before;
 import org.junit.Test;
 import repeat.Repeat;
 
@@ -10,12 +11,18 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class BetterRadarTest {
+    Executor executor;
+    PatriotBattery batteryMock;
+
+    @Before
+    public void setUp() {
+        executor = command -> command.run();
+        batteryMock = mock(PatriotBattery.class);
+    }
+
     @Test
     @Repeat(times = 2000)
     public void launchPatriotTenTimesWhenNoticesAScudMissile() {
-        Executor executor = command -> command.run();
-
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar radar = new BetterRadar(batteryMock, 10, executor);
 
         radar.notice(new Scud());
@@ -26,9 +33,6 @@ public class BetterRadarTest {
     @Test
     @Repeat(times = 2000)
     public void launchPatriotOnceWhenNoticesAScudMissile() {
-        Executor executor = command -> command.run();
-
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar radar = new BetterRadar(batteryMock, 1, executor);
 
         radar.notice(new Scud());
