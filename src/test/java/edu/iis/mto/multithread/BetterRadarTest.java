de02ec8dddde3bd38826c8c5c8d2b5ce93648266
Marcin Kapelan <marcinkape@gmail.com>
2018-06-05 13:19:31 +0200

    Adapt tests to changes

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -3,8 +3,7 @@ package edu.iis.mto.multithread;
 import org.junit.Test;
 import repeat.Repeat;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.Executor;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -13,18 +12,27 @@ import static org.mockito.Mockito.verify;
 public class BetterRadarTest {
     @Test
     @Repeat(times = 2000)
-    public void launchPatriotCorrectNumberOfTimesWhenNoticesAScudMissle() {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+    public void launchPatriotTenTimesWhenNoticesAScudMissile() {
+        Executor executor = command -> command.run();
 
         PatriotBattery batteryMock = mock(PatriotBattery.class);
-        BetterRadar radar = new BetterRadar(batteryMock, executorService);
+        BetterRadar radar = new BetterRadar(batteryMock, 10, executor);
 
         radar.notice(new Scud());
 
-        executorService.shutdown();
-        while (!executorService.isTerminated()) {
-        }
-
         verify(batteryMock, times(10)).launchPatriot();
+    }
+
+    @Test
+    @Repeat(times = 2000)
+    public void launchPatriotOnceWhenNoticesAScudMissile() {
+        Executor executor = command -> command.run();
+
+        PatriotBattery batteryMock = mock(PatriotBattery.class);
+        BetterRadar radar = new BetterRadar(batteryMock, 1, executor);
+
+        radar.notice(new Scud());
+
+        verify(batteryMock, times(1)).launchPatriot();
     }
 }
